73428705f038f5aa63496a7244411add0be0412a
Predrag Vlajkovic <predrag.vlajkovic@gmail.com>
2018-05-22 12:33:21 +0200

    Fixed services seed

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -18,21 +18,21 @@ class ServiceSeeder extends Seeder
         	'active'=>'1', 'status'=>'1', 
         	'status_change'=>Carbon::now(),         	
         	'last_status_down'=>Carbon::now(), 'last_status_up'=>Carbon::now(),
-        	'username'=>'', 'password'=>'', 
+        	'user'=>'', 'pass'=>'', 
         	'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()]);
         Service::insert (['id'=>2, 'name'=>'SoftwarePieces web site', 
         	'host_id'=>'1', 'probe_id'=>'4', 'port'=>'', 'uri'=>'', 
         	'active'=>'1', 'status'=>'1', 
         	'status_change'=>Carbon::now(),         	
         	'last_status_down'=>Carbon::now(), 'last_status_up'=>Carbon::now(),
-        	'username'=>'', 'password'=>'', 
+            'user'=>'', 'pass'=>'', 
         	'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()]); 
         Service::insert (['id'=>3, 'name'=>'Jatheon web site', 
         	'host_id'=>'3', 'probe_id'=>'2', 'port'=>'443', 'uri'=>'', 
         	'active'=>'1', 'status'=>'0', 
         	'status_change'=>Carbon::now(),         	
         	'last_status_down'=>Carbon::now(), 'last_status_up'=>Carbon::now(),
-        	'username'=>'', 'password'=>'', 
+        	'user'=>'', 'pass'=>'', 
         	'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()]);
     }
 }
